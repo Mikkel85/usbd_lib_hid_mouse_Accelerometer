ef6dab1f15261af06772a9e638be963f0fa659ca
Mikkel85 <sgt.mich@gmail.com>
2018-05-04 08:54:48 +0200

    working usb mouse devise whit 5 functions

---
+++ Debug/example/src/hid_desc.d
@@ -0,0 +1,46 @@
+example/src/hid_desc.o example/src/hid_desc.d: ../example/src/hid_desc.c \
+ C:\Users\user\Documents\MCUXpressoIDE_10.0.2_411\workspace\usbd_lib_hid_mouse_Accelerometer\example\inc/app_usbd_cfg.h \
+ C:\Users\user\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_chip_40xx\inc/lpc_types.h \
+ C:\Users\user\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_chip_40xx\inc/error.h \
+ C:\Users\user\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_chip_40xx\inc\usbd/usbd_rom_api.h \
+ C:\Users\user\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_chip_40xx\inc\usbd/usbd.h \
+ C:\Users\user\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_chip_40xx\inc\usbd/usbd_hw.h \
+ C:\Users\user\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_chip_40xx\inc\usbd/usbd_core.h \
+ C:\Users\user\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_chip_40xx\inc\usbd/usbd_mscuser.h \
+ C:\Users\user\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_chip_40xx\inc\usbd/usbd_msc.h \
+ C:\Users\user\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_chip_40xx\inc\usbd/usbd_dfuuser.h \
+ C:\Users\user\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_chip_40xx\inc\usbd/usbd_dfu.h \
+ C:\Users\user\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_chip_40xx\inc\usbd/usbd_hiduser.h \
+ C:\Users\user\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_chip_40xx\inc\usbd/usbd_hid.h \
+ C:\Users\user\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_chip_40xx\inc\usbd/usbd_cdcuser.h \
+ C:\Users\user\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_chip_40xx\inc\usbd/usbd_cdc.h
+
+C:\Users\user\Documents\MCUXpressoIDE_10.0.2_411\workspace\usbd_lib_hid_mouse_Accelerometer\example\inc/app_usbd_cfg.h:
+
+C:\Users\user\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_chip_40xx\inc/lpc_types.h:
+
+C:\Users\user\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_chip_40xx\inc/error.h:
+
+C:\Users\user\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_chip_40xx\inc\usbd/usbd_rom_api.h:
+
+C:\Users\user\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_chip_40xx\inc\usbd/usbd.h:
+
+C:\Users\user\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_chip_40xx\inc\usbd/usbd_hw.h:
+
+C:\Users\user\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_chip_40xx\inc\usbd/usbd_core.h:
+
+C:\Users\user\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_chip_40xx\inc\usbd/usbd_mscuser.h:
+
+C:\Users\user\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_chip_40xx\inc\usbd/usbd_msc.h:
+
+C:\Users\user\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_chip_40xx\inc\usbd/usbd_dfuuser.h:
+
+C:\Users\user\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_chip_40xx\inc\usbd/usbd_dfu.h:
+
+C:\Users\user\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_chip_40xx\inc\usbd/usbd_hiduser.h:
+
+C:\Users\user\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_chip_40xx\inc\usbd/usbd_hid.h:
+
+C:\Users\user\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_chip_40xx\inc\usbd/usbd_cdcuser.h:
+
+C:\Users\user\Documents\MCUXpressoIDE_10.0.2_411\workspace\lpc_chip_40xx\inc\usbd/usbd_cdc.h:
